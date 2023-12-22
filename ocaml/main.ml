29ee37692f3918408dff42355807f34e5e1e4779
BossyRomain <romain.bossy.01@gmail.com>
2023-12-22 15:06:55 +0100

    Update gitignore

--- ocaml/main.ml
+++ ocaml/main.ml
@@ -8,21 +8,14 @@ let file f =
     (*let env = Typechecker.predef in
     let liste_equation = Typechecker.genEquations ast env Int in
     let res = Typechecker.to_string liste_equation in*)
-    Typechecker.type_check(ast);
+    (* Typechecker.type_check(ast); *)
     (*Printf.printf("Resultat\n");*)
     (*Printf.printf("%s") res;*)
 
-    (*let table = (Syntax.find_variables ast []) in
-    let c_ast = (K_normalization.k_normalization (Syntax.clone_ast ast)) in
-    (* let table = (Syntax.find_variables ast []) in
-    Printf.printf "Table des symboles\n";
-    List.iter (Printf.printf "%s ") res;
-    print_newline ();*)
-    List.iter (Printf.printf "%s ") table;
-    print_newline (); *)
-    print_ast ast;
-    print_newline ();
-    print_ast c_ast;
+    (*let table = (Syntax.find_variables ast []) in *)
+    let c_ast = (Closure.conversion (K_normalization.k_normalization (Syntax.clone_ast ast))) in
+    Printf.printf("%s\n") (Closure.to_string c_ast);
+    
     close_in inchan
   with e -> (close_in inchan; raise e)
 
