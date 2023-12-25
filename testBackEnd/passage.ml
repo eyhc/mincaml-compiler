d56fa5f89b37805acb89e15aa713fc034a5d88f3
Serge Hakobyan <hakobyas@univ-grenoble-alpes.fr>
2023-12-26 00:02:45 +0100

    added functions for lists + spillAtInterval

--- testBackEnd/passage.ml
+++ testBackEnd/passage.ml
@@ -1,196 +1,241 @@
-(* `Code` est un ensemble de fonctions *)
-(* type asml =
-  | Code of asml_expr list *)
+(* Ceci est un éditeur pour OCaml
+   Entrez votre programme ici, et envoyez-le au toplevel en utilisant le
+   bouton "Évaluer le code" ci-dessous ou [Ctrl-e]. *)
 
-  type asml_expr =
-  | Var of string
-  | Int of int
-  | RegLet of string * asml_expr
-  | Let of string * asml_expr
-  (* | StackLet of  *)
-  | Fun of asm_function
-  | Add of asml_expr * asml_expr
-  | Sub of asml_expr * asml_expr
-  | Mul of asml_expr * asml_expr
-  | Assign of string * asml_expr
-
-and asm_function = {
-  name : string;
-  params : asml_expr list;
-  body : asml_expr list;
-}
-
-let asml = 
-  [
-    Fun {
-      name = "_";
-      params = [];
-      body = [Let ("x", Int 1); Let ("y", Int 2); Let ("a", Int 8);
-              Let ("b", Int 14); Assign ("a", Sub (Var "x", Var "y"))
-             ;Assign ("z", Add (Var "x", Var "y"));Assign ("b", Mul (Var "a", Var "z"))];
-    }
-  ]
-;;
-
-
-let asml_show asml = 
-  let rec asml_show_unit asml = 
-    match asml with
-    | Var v -> Printf.printf "%s" v
-    | Int i -> Printf.printf "%d" i
-    | RegLet (r, e) -> 
-        Printf.printf "Register Let: %s\n" r;
-        asml_show_unit e
-    | Let (l, e) -> 
-        Printf.printf "Let %s = " l;
-        asml_show_unit e;
-        Printf.printf "\n"
-    | Assign (s, e) -> 
-        Printf.printf "%s = " s;
-        asml_show_unit e;
-        Printf.printf "\n"
-    | Add (e1, e2) ->  
-        asml_show_unit e1;
-        Printf.printf " + ";
-        asml_show_unit e2;
-        Printf.printf "\n";    
-    | Mul (e1, e2) ->  
-        asml_show_unit e1;
-        Printf.printf " * ";
-        asml_show_unit e2;
-        Printf.printf "\n";
-    | Sub (e1, e2) ->  
-        asml_show_unit e1;
-        Printf.printf " - ";
-        asml_show_unit e2;
-        Printf.printf "\n";
-    | Fun f ->
-        Printf.printf "Function: %s\n" f.name;
-        Printf.printf "Params:\n";
-        List.iter (fun param -> asml_show_unit param; Printf.printf "---\n") f.params;
-        Printf.printf "Body:\n";
-        List.iter (fun expr -> asml_show_unit expr; Printf.printf "---\n") f.body
-    | _ -> () (* Handle other cases as needed *)
-  
-  and 
-  
-    asml_list_show l = 
-    match l with 
-    | e :: ll -> asml_show_unit e; asml_list_show ll;
-    | [] -> () 
-  in asml_list_show asml
-;;
-
-(* let rec asml_list_apply func l = 
-  match l with 
-  | e :: ll -> func e; asml_list_apply func ll
-  | [] -> ()
-;; *)
-
-let key_in_hash hash key =
-  try 
-    Hashtbl.find hash key;
-    true
-  with e -> (false)
-;;
-
-let add_hash hash key value =
-  if not (key_in_hash hash key) then 
-    Hashtbl.add hash key value;
-  hash
-;;
-
-let modify_value hashmap key value =
-  match (Hashtbl.find_opt hashmap key) with
-  | Some lst -> Hashtbl.replace hashmap key (List.hd lst :: value :: []); hashmap
-  | None -> Hashtbl.add hashmap key [value; value]; hashmap
-;;
-
-let get_vars asml =
-  let rec g_v asml l hash ind =
-    match asml with 
-    | Var v ->  
-        if not (key_in_hash hash v) then 
-          (l @ [v, ind], modify_value hash v ind)
-        else
-          (l, modify_value hash v ind)
-    | Let (s, e) ->
-        g_v e l hash (ind + 1)
-    | Assign (s, e) -> 
-        if not (key_in_hash hash s) then 
-          g_v e (l @ [(s, ind)]) (modify_value hash s (ind) ) (ind) 
-        else 
-          g_v e l (modify_value hash s (ind) ) (ind)
-    | Add (e1, e2) | Mul (e1, e2) | Sub (e1, e2) -> 
-        g_v e2 (l @ (fst (g_v e1 [] hash (ind)))) hash (ind)
-    | Fun f ->  iter_list f.body l hash ind
-    | _ -> (l, hash) (* Handle other cases as needed *)
-    
-  and 
-    iter_list asml_list l hash ind =
-    match asml_list with 
-    | e :: ll -> 
-        let hash_ = (g_v e l hash ind) in
-        iter_list ll (fst hash_) (snd hash_) (ind + 1)
-    | [] -> (l, hash)
-  in
-  iter_list asml [] (Hashtbl.create 0) 1
-;;
-
-let rec add_to_list_in_order list value cmp = 
-  match list with 
-  | [] -> value :: []
-  | e :: ll -> 
-      (* snd value <= snd e *)
-      if cmp (snd value) (snd e) then value :: e :: ll else e :: add_to_list_in_order ll value cmp 
-;;
-
-(* 1 takes the name of the variable form a (string * int) list 
-   2 takes its `end` value from the hashtable
-   3 *)
-let start_to_end list_start list_end hash =
-  let var_name = fst (List.hd list_start) in 
-  let var = (var_name, (List.nth (Hashtbl.find hash var_name) 1)) in
-  Printf.printf "(\"%s\", %d); " (fst var) (snd var);
-  add_to_list_in_order list_end var ( <= )
-;;
-
-let create_and_initialize_register_hashtable () =
-  let my_hashtable = Hashtbl.create 9 in
-  for i = 4 to 12 do
-    Hashtbl.add my_hashtable ("r" ^ string_of_int i) 1
-  done;
-  my_hashtable
-;;
-
-let find_free_register hashtable =
-  let found_key = ref None in
-  Hashtbl.iter (fun key value ->
-      if value = 1 && !found_key = None then begin
-        found_key := Some key;
-        Hashtbl.replace hashtable key 0
-      end
-    ) hashtable;
-  !found_key
-;;
-
-
-
-let registers = create_and_initialize_register_hashtable () ;;
-let (var_start_list, hashtable) = get_vars asml ;;
-Hashtbl.iter (fun a b ->Printf.printf "\"%s\", %d %d \n" a (List.hd b) (List.nth b 1)) hashtable;;
-List.iter (fun a -> Printf.printf "(\"%s\", %d); " (fst a) (snd a)) var_start_list;;
-
-let var_end_list = [("x", 6); ("y", 6);  ("a", 7); ("z", 7); ("b", 7)];;
-
-start_to_end var_start_list  var_end_list hashtable
+   type asml_expr =
+   | Var of string
+   | Int of int
+   | RegLet of string * asml_expr
+   | Let of string * asml_expr
+   (* | StackLet of  *)
+   | Fun of asm_function
+   | Add of asml_expr * asml_expr
+   | Sub of asml_expr * asml_expr
+   | Mul of asml_expr * asml_expr
+   | Assign of string * asml_expr
  
-
-
-(* asml_list_apply asml_show asml;; *)
-(*asml_show asml;;*)
-
-
-(* let linearScanAlloc hashmap var_list =  *)
-  
-  
+ and asm_function = {
+   name : string;
+   params : asml_expr list;
+   body : asml_expr list;
+ }
+ ;;
+ 
+ let asml_show asml = 
+   let rec asml_show_unit asml = 
+     match asml with
+     | Var v -> Printf.printf "%s" v
+     | Int i -> Printf.printf "%d" i
+     | RegLet (r, e) -> 
+         Printf.printf "Register Let: %s\n" r;
+         asml_show_unit e
+     | Let (l, e) -> 
+         Printf.printf "Let %s = " l;
+         asml_show_unit e;
+         Printf.printf "\n"
+     | Assign (s, e) -> 
+         Printf.printf "%s = " s;
+         asml_show_unit e;
+         Printf.printf "\n"
+     | Add (e1, e2) ->  
+         asml_show_unit e1;
+         Printf.printf " + ";
+         asml_show_unit e2;
+         Printf.printf "\n";
+     | Fun f ->
+         Printf.printf "Function: %s\n" f.name;
+         Printf.printf "Params:\n";
+         List.iter (fun param -> asml_show_unit param; Printf.printf "---\n") f.params;
+         Printf.printf "Body:\n";
+         List.iter (fun expr -> asml_show_unit expr; Printf.printf "---\n") f.body
+     | _ -> () (* Handle other cases as needed *)
+   
+   and 
+   
+     asml_list_show l = 
+     match l with 
+     | e :: ll -> asml_show_unit e; asml_list_show ll;
+     | [] -> () 
+   in asml_list_show asml
+ ;;
+ 
+ 
+ (* let get_intervals asml =
+   let rec g_v asml l  ind =
+     match asml with 
+     | Var v -> (l @ [v], modify_value hash v ind)
+     | Int i -> (l, hash)
+     | Let (s, e) ->
+         g_v e (l @ [s])  (ind + 1)
+     | Assign (s, e) -> 
+         g_v e (l @ [s]) (modify_value hash s ind ) (ind + 1)
+     | Add (e1, e2) -> g_v e2 (l @ (fst (g_v e1 [] (ind))) ) (ind)
+     | Mul (e1, e2) -> g_v e2 (l @ (fst (g_v e1 [] (ind))) ) (ind)
+     | Sub (e1, e2) -> g_v e2 (l @ (fst (g_v e1 [] (ind))) ) (ind)
+     | Fun f ->  iter_list f.body l
+         (* List.iter (fun param -> g_v param ) f.params; *)
+         (* List.iter (fun expr -> g_v expr ) f.body *)
+     | _ -> (l, hash) (* Handle other cases as needed *)
+     
+   and 
+     iter_list asml_list l =
+     match asml_list with 
+     | e :: ll -> 
+         let hash_ = (g_v e l hash 0) in
+         iter_list ll 
+     | [] -> l
+   in
+   iter_list asml [] (Hashtbl.create 0)
+ ;; *)
+ 
+ let asml = 
+   [
+     Fun {
+       name = "_";
+       params = [];
+       body = [Let ("x", Int 1); Let ("y", Int 2); Let ("a", Int 8);
+               Let ("b", Int 14); Assign ("a", Sub (Var "x", Var "y"));
+               Assign ("z", Add (Var "x", Var "y"));Assign ("b", Mul (Var "a", Var "z"))];
+     }
+   ]
+ ;;
+ 
+ let rec find_var_and_replace lst var use =
+   match lst with
+   | (v, (first_use, last_use)) :: tl ->
+       if v = var then
+         (v, (first_use, use)) :: tl
+       else
+         (v, (first_use, last_use)) :: find_var_and_replace tl var use
+   | [] -> (var, (use, use)) :: []
+ ;;
+ 
+ let get_intervals asml = 
+   let rec gv asml l ind =
+     match asml with 
+       (* if variable exists in the list `l`, will update it's `last use` value*)
+     | Var v -> find_var_and_replace l v ind 
+     | Let (s, e) -> 
+         let k = gv e l (ind + 1) in
+         find_var_and_replace k s (ind + 1)  
+     | Assign (s, e) -> 
+         let k = gv e l (ind + 1) in
+         find_var_and_replace k s (ind + 1)  
+     | Add (e1, e2) | Mul (e1, e2) | Sub (e1, e2) -> 
+         gv e1 (gv e2 l ind) ind
+     | Fun f -> iter_list f.body l ind
+     | _ -> l
+   and 
+     iter_list asml_list l ind = 
+     match asml_list with
+     | e :: ll -> iter_list ll (gv e l ind)  (ind + 1)
+     | [] -> l
+   in iter_list asml [] 0
+ ;;
+ 
+ let intervals = get_intervals asml;;
+ (* number of free registers *)
+ let num_free = 9;;
+ (* list of free registers *)
+ let free = ["r4"; "r5"; "r6"; "r7"; "r8"; "r9"; "r10"; "r11"; "r12"];;
+ (* 
+   hashtable binding variable to register,
+   a : r1 -> variable `a` is stored in register `r1` 
+ *)
+ let var_to_register = Hashtbl.create num_free;;
+ 
+ let var_name (a, (_, _)) = a;;
+ let startpoint (_, (b, _)) = b;;
+ let endpoint (_, (_, c)) = c;;
+ 
+ (* ---------------Functions with lists--------------- *)
+ exception EOL;;
+ 
+ let rec remove_el el = function
+   | e :: ll -> 
+       if e == el then 
+         ll
+       else 
+         e :: remove_el el ll
+   | [] -> []
+ ;;
+ 
+ let rec get_last_el = function
+   | [] -> [] 
+   | e :: [] -> e
+   | e :: ll -> get_last_el ll
+ ;;
+ 
+ (* Returns a list containing all the variable declarations on the given interval *)
+ let rec get_new_var_on_interval interval = function
+   | e :: ll ->
+       if (endpoint e) < interval then
+         get_new_var_on_interval interval ll
+       else if interval = (endpoint e) then
+         e :: get_new_var_on_interval interval ll
+       else 
+         []
+   | [] -> raise EOL
+ ;;
+ 
+ (* Adds element to the list sorted by its start/end point *)
+ let rec add_by_increasing_func_point func el = function
+   | e :: ll ->
+       if (func el) <= (func e) then
+         el :: e :: ll
+       else 
+         e :: add_by_increasing_func_point func el ll
+   | [] -> el :: []
+ ;;
+ 
+ (* Sorts list by start/end point *)
+ let sort_func_point liste func =
+   let comparer a b f =
+     compare (f a) (f b) 
+   in
+   List.sort (fun x y -> comparer x y func) liste
+ ;;
+ 
+ (* 
+ Takes `var` -> variable's name
+   Returns `loc` : FP - loc, the location in the memory 
+                                             *)
+ let var_stack_location var = 
+   let rec vsl loc = function
+     | e :: ll -> 
+         if (var_name e) = var then
+           loc
+         else vsl (loc + 4) ll
+     | [] -> raise EOL
+   in vsl 4 intervals
+ ;;
+ (* ------------Fin Functions with lists------------ *)
+ 
+ 
+ let spillAtInterval active i = 
+   let spill = get_last_el active in
+   let register = Hashtbl.find var_to_register (var_name i) in
+   (* remove spill from active *)
+   let act = remove_el spill active in
+   if (endpoint spill) > (endpoint i) then begin
+     (* register[i] ← register[spill] *)
+     Hashtbl.remove active (var_name i);
+     Hashtbl.add active (var_name spill) register;
+     
+     (* location[spill] ← new stack location *)
+     (* ??? *)    
+ 
+     (* add i to active, sorted by increasing end point *)
+     add_by_increasing_func_point endpoint i act
+   end
+   else
+     (* location[i] ← new stack location *)
+     (* ??? *)
+ 
+     active 
+ end
+ ;;
+ 
+ (* asml_list_apply asml_show asml;; *)
+ (*asml_show asml;;*) 
+ 
