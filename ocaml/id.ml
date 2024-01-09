64b1c79c6bae2f3aa939fb0dbc3bdb6a78f561de
carrote <carrote@etu.univ-grenoble-alpes.fr>
2024-01-09 11:36:49 +0100

    changement nommage des nouvelles variables dans id.ml

--- ocaml/id.ml
+++ ocaml/id.ml
@@ -3,15 +3,10 @@ type l = string
 
 let to_string x = x
 
-let genid =
-  let counter = ref (0) in
-  fun () ->
-    incr counter;
-    Printf.sprintf "v%d_" !counter
-
 let make_unique =
   let counter = ref (0) in
   fun (name:t) : t ->
-    if String.get name ((String.length name) - 1) = '_' then name
-    else 
-      let _ = incr counter in Printf.sprintf "%s%d" name !counter
+    incr counter;
+    Printf.sprintf "%s%d" name !counter
+
+let genid () = make_unique "temp"
