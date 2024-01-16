37aa416cf9d80c11d2dde22716010fd5ae07b3c8
Elie Carrot <eyh.carrot@gmail.com>
2024-01-16 14:49:06 +0100

    correction dans elim.ml

--- ocaml/elim.ml
+++ ocaml/elim.ml
@@ -5,6 +5,23 @@ Elimination of unnecessary definitions
 date : 09-01-2023
 *)
 
+(* return true iff expression contains side effect *)
+(* env is the list of functions with side effects *)
+let rec has_side_effects (a:Knorm.knorm_t) (env:Id.t list) : bool =
+  match a with
+  | App (f, vars) -> List.exists (fun x -> x = f) env
+  | Put _ -> true
+  | IfEq (_,e1,e2) | IfLE (_,e1,e2) | Let (_, e1, e2) ->
+    (has_side_effects e1 env) || (has_side_effects e2 env) 
+  | LetRec (fd, e) -> 
+    if has_side_effects fd.body env then
+      let env2 = (fst fd.name)::env in
+        has_side_effects e env2
+    else has_side_effects e env
+  | LetTuple (l1, v, e) -> has_side_effects e env
+  | _ -> false
+
+
 type environment = (Id.t * int ref) list
 
 (* increment of var v in env *)
@@ -19,12 +36,19 @@ let rec env_get (env:environment) (v:Id.t) : int =
   | (x,i)::l ->
     if x = v then !i else env_get l v
 
+
+let predef_with_side_effets: Id.t list = [
+  "print_int";
+  "print_float";
+  "print_newline";
+]
+
 (* Main function *)
 let elim_definition (ast:Knorm.knorm_t) : Knorm.knorm_t =
-  let rec elim (a:Knorm.knorm_t) (env:environment) : Knorm.knorm_t =
+  let rec elim (a:Knorm.knorm_t) (env:environment) (env2:Id.t list) : Knorm.knorm_t =
     match a with
     | Unit | Int _ | Float _ -> a
-  
+
     (* no recursive calls *)
     | Var s -> let _ = env_incr env s in a
     | Neg x | FNeg x -> let _ = env_incr env x in a
@@ -59,32 +83,37 @@ let elim_definition (ast:Knorm.knorm_t) : Knorm.knorm_t =
     | IfEq ((x,y),e1,e2) -> 
       let _ = env_incr env x in 
         let _ = env_incr env y in 
-          IfEq ((x,y), elim e1 env, elim e2 env)
+          IfEq ((x,y), elim e1 env env2, elim e2 env env2)
     | IfLE ((x,y),e1,e2) -> 
       let _ = env_incr env x in 
         let _ = env_incr env y in 
-          IfLE ((x,y), elim e1 env, elim e2 env)
+          IfLE ((x,y), elim e1 env env2, elim e2 env env2)
 
     (* Main cases *)
     | Let ((s,t), e1, e2) -> 
-      (match e1 with 
-      | Put _ -> a (* here there is side effect *)
-      | _ ->
-        let env' = (s, ref 0)::env in
-          let e1' = elim e1 env and e2' = elim e2 env' in
-            if (env_get env' s) <> 0 then Let((s,t), e1', e2') else e2')
+      (* here CAUTION of side effects *)
+      let env' = (s, ref 0)::env in        
+        let e1' = elim e1 env env2 and e2' = elim e2 env' env2 in
+          if (env_get env' s) <> 0 || has_side_effects e1' env2 then 
+            Let((s,t), e1', e2')
+          else e2'
 
     | LetRec (fd, e) -> 
       let env' = (fst fd.name, ref 0)::env in
-        let b = elim fd.body env and e' = elim e env' in
-        if (env_get env' (fst fd.name)) <> 0 then
-          LetRec({name= fd.name; args=fd.args; body=b}, e')
-        else e'
+        let b = elim fd.body env env2 in
+          let env22 = 
+            if has_side_effects b env2 then (fst fd.name)::env2 else env2
+          in 
+            let e' = elim e env' env22 in
+              if (env_get env' (fst fd.name)) <> 0 then
+                LetRec({name= fd.name; args=fd.args; body=b}, e')
+              else e'
+
     | LetTuple (l1, v, e) -> 
       let _ = env_incr env v in
         let env' = (List.map (fun x -> (fst x, ref 0)) l1) @ env in
-          let e' = elim e env' in
+          let e' = elim e env' env2 in
             if (List.fold_left (fun x y -> x + (env_get env' (fst y))) 0 l1) <> 0 then
               LetTuple(l1, v, e')
             else e'
-  in elim ast []
+  in elim ast [] predef_with_side_effets
