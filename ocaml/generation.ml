70dbfbc583b025f67a66a642c9217c101cdb2eb9
AliBay07 <ali_bay44@outlook.com>
2024-01-19 20:10:25 +0100

    removed useless functions from generation

--- ocaml/generation.ml
+++ ocaml/generation.ml
@@ -16,90 +16,34 @@ let generate_num_label () =
   let label = "const_" ^ string_of_int !num_label_counter in
   num_label_counter := !num_label_counter + 1;
   label
-
-let rec count_lets_in_regt : regt -> int = function
-| Let (s, _) when List.mem s ["r0"; "r1"; "r2"; "r3"; "sp"] -> 0
-| Let (s, If (_, _, rts1, rts2)) ->
-  count_lets_in_regt_list rts1 + count_lets_in_regt_list rts2
-| Exp (If (_, _, rts1, rts2)) ->
-  count_lets_in_regt_list rts1 + count_lets_in_regt_list rts2
-| Let (s, _) -> 1
-| _ -> 0
-
-and count_lets_in_regt_list : regt list -> int = fun rts ->
-  List.fold_left (fun acc rt -> acc + count_lets_in_regt rt) 0 rts
-
-let rec count_lets_in_reg_function : reg_function -> int =
-  fun { body; _ } -> List.fold_left (fun acc stmt -> acc + count_lets_in_regt stmt) 0 body
    
-  and generate_asm_regt : regt -> string list = function
-  | Let (s, expr) ->
-    (match expr with
-      | Int n ->
+let rec generate_asm_regt : regt -> string list = function
+| Let (s, expr) ->
+  (match expr with
+    | Int n ->
+      if n <= 255 then
+        [Printf.sprintf "\tmov %s, #%d" s n]
+      else
+        [Printf.sprintf "\tldr %s, =#%d" s n]
+    | Reg reg -> [Printf.sprintf "\tmov %s, %s" s reg]
+    | Add (s1, expr) ->
+      (match expr with
+      | Int n -> 
         if n <= 255 then
-          [Printf.sprintf "\tmov %s, #%d" s n]
+          [Printf.sprintf "\tadd %s, %s, #%d" s s1 n]
         else
-          [Printf.sprintf "\tldr %s, =#%d" s n]
-      | Reg reg -> [Printf.sprintf "\tmov %s, %s" s reg]
-      | Add (s1, expr) ->
-        (match expr with
-        | Int n -> 
-          if n <= 255 then
-            [Printf.sprintf "\tadd %s, %s, #%d" s s1 n]
-          else
-            [Printf.sprintf "\tldr r12, =#%d" n; Printf.sprintf "\tadd %s, %s, r12" s s1]
-        | Reg reg -> [Printf.sprintf "\tadd %s, %s, %s" s s1 reg]
-        | _ -> assert false)
-      | Sub (s1, expr) ->
-        (match expr with
-        | Int n -> 
-          if n <= 255 then
-            [Printf.sprintf "\tsub %s, %s, #%d" s s1 n]
-          else
-            [Printf.sprintf "\tldr r12, =#%d" n; Printf.sprintf "\tsub %s, %s, r12" s s1]
-        | Reg reg -> [Printf.sprintf "\tsub %s, %s, %s" s s1 reg]
-        | _ -> assert false)
-      | If (cmp_type, (r1, Reg r2), true_branch, false_branch) ->
-        let true_label = generate_if_label () in
-        let end_label = generate_if_label () in
-        Printf.sprintf "\tcmp %s, %s" r1 r2 ::
-        Printf.sprintf "\tb%s %s" cmp_type true_label ::
-        List.concat (List.map generate_asm_regt false_branch) @
-        Printf.sprintf "\tb %s" end_label ::
-        Printf.sprintf "\t%s:" true_label ::
-        List.concat (List.map generate_asm_regt true_branch) @
-        Printf.sprintf "\t%s:" end_label ::
-        Printf.sprintf "\tmov %s, r0" s :: []
-      
-      | If (cmp_type, (r1, Int n), true_branch, false_branch) ->
-        let true_label = generate_if_label () in
-        let end_label = generate_if_label () in
-        let cmp_instruction =
-          if n <= 255 then
-            Printf.sprintf "\tcmp %s, #%d" r1 n
-          else
-            Printf.sprintf "\tldr r12, =%d\n\tcmp %s, r12" n r1
-        in
-        cmp_instruction ::
-        Printf.sprintf "\tb%s %s" cmp_type true_label ::
-        List.concat (List.map generate_asm_regt false_branch) @
-        Printf.sprintf "\tb %s" end_label ::
-        Printf.sprintf "\t%s:" true_label ::
-        List.concat (List.map generate_asm_regt true_branch) @
-        Printf.sprintf "\t%s:" end_label ::
-        Printf.sprintf "\tmov %s, r0" s :: []
-      | Call (func_name, nb_params) ->
-        [Printf.sprintf "\tpush {r0-r10}";
-          Printf.sprintf "\tbl %s" func_name;
-          Printf.sprintf "\tmov r12, r0";
-          Printf.sprintf "\tpop {r0-r10}";
-          Printf.sprintf "\tmov r0, r12";
-          Printf.sprintf "\tmov %s, r0" s]   
-      | Neg s1 -> [Printf.sprintf "\tneg %s, %s" s s1]
-      | Unit -> [Printf.sprintf "\tmov %s, #0" s]
+          [Printf.sprintf "\tldr r12, =#%d" n; Printf.sprintf "\tadd %s, %s, r12" s s1]
+      | Reg reg -> [Printf.sprintf "\tadd %s, %s, %s" s s1 reg]
       | _ -> assert false)
-  | Exp exp ->
-  (match exp with
+    | Sub (s1, expr) ->
+      (match expr with
+      | Int n -> 
+        if n <= 255 then
+          [Printf.sprintf "\tsub %s, %s, #%d" s s1 n]
+        else
+          [Printf.sprintf "\tldr r12, =#%d" n; Printf.sprintf "\tsub %s, %s, r12" s s1]
+      | Reg reg -> [Printf.sprintf "\tsub %s, %s, %s" s s1 reg]
+      | _ -> assert false)
     | If (cmp_type, (r1, Reg r2), true_branch, false_branch) ->
       let true_label = generate_if_label () in
       let end_label = generate_if_label () in
@@ -109,8 +53,8 @@ let rec count_lets_in_reg_function : reg_function -> int =
       Printf.sprintf "\tb %s" end_label ::
       Printf.sprintf "\t%s:" true_label ::
       List.concat (List.map generate_asm_regt true_branch) @
-      Printf.sprintf "\tb %s" end_label ::
-      Printf.sprintf "\t%s:" end_label :: []
+      Printf.sprintf "\t%s:" end_label ::
+      Printf.sprintf "\tmov %s, r0" s :: []
     
     | If (cmp_type, (r1, Int n), true_branch, false_branch) ->
       let true_label = generate_if_label () in
@@ -127,44 +71,85 @@ let rec count_lets_in_reg_function : reg_function -> int =
       Printf.sprintf "\tb %s" end_label ::
       Printf.sprintf "\t%s:" true_label ::
       List.concat (List.map generate_asm_regt true_branch) @
-      Printf.sprintf "\t%s:" end_label :: [] 
-    | Int n ->
-      if n <= 255 then
-        [Printf.sprintf "\tmov r0, #%d" n]
-      else
-        [Printf.sprintf "\tldr r0, =#%d" n]      
-    | Reg reg -> [Printf.sprintf "\tmov r0, %s" reg]
-    | Add (s1, expr) ->
-      (match expr with
-      | Int n -> 
-        if n <= 255 then
-          [Printf.sprintf "\tadd r0, %s, #%d" s1 n]
-        else
-          [Printf.sprintf "\tldr r0, =#%d" n; Printf.sprintf "\tadd r0, %s, r0" s1]
-      | Reg reg -> [Printf.sprintf "\tadd r0, %s, %s" s1 reg]
-      | _ -> assert false)
-    | Sub (s1, expr) ->
-      (match expr with
-      | Int n -> 
-        if n <= 255 then
-          [Printf.sprintf "\tsub r0, %s, #%d" s1 n]
-        else
-          [Printf.sprintf "\tldr r0, =#%d" n; Printf.sprintf "\tsub r0, %s, r0" s1]
-      | Reg reg -> [Printf.sprintf "\tsub r0, %s, %s" s1 reg]
-      | _ -> assert false)
+      Printf.sprintf "\t%s:" end_label ::
+      Printf.sprintf "\tmov %s, r0" s :: []
     | Call (func_name, nb_params) ->
-        [Printf.sprintf "\tpush {r0-r10}";
-          Printf.sprintf "\tbl %s" func_name;
-          Printf.sprintf "\tmov r12, r0";
-          Printf.sprintf "\tpop {r0-r10}";
-          Printf.sprintf "\tmov r0, r12"]
-    | Neg s1 -> [Printf.sprintf "\tneg r0, %s" s1]
-    | Unit -> []
+      [Printf.sprintf "\tpush {r0-r10}";
+        Printf.sprintf "\tbl %s" func_name;
+        Printf.sprintf "\tmov r12, r0";
+        Printf.sprintf "\tpop {r0-r10}";
+        Printf.sprintf "\tmov r0, r12";
+        Printf.sprintf "\tmov %s, r0" s]   
+    | Neg s1 -> [Printf.sprintf "\tneg %s, %s" s s1]
+    | Unit -> [Printf.sprintf "\tmov %s, #0" s]
     | _ -> assert false)
-  | Store (Reg reg, mem) -> [Printf.sprintf "\tstr %s, [fp, #%s]" reg mem]
-  | Load (s, Reg reg) -> [Printf.sprintf "\tldr %s, [fp, #%s]" reg s]
-  | Push (r) -> [Printf.sprintf "\tpush {%s}" r]
-  | _ -> assert false
+| Exp exp ->
+(match exp with
+  | If (cmp_type, (r1, Reg r2), true_branch, false_branch) ->
+    let true_label = generate_if_label () in
+    let end_label = generate_if_label () in
+    Printf.sprintf "\tcmp %s, %s" r1 r2 ::
+    Printf.sprintf "\tb%s %s" cmp_type true_label ::
+    List.concat (List.map generate_asm_regt false_branch) @
+    Printf.sprintf "\tb %s" end_label ::
+    Printf.sprintf "\t%s:" true_label ::
+    List.concat (List.map generate_asm_regt true_branch) @
+    Printf.sprintf "\tb %s" end_label ::
+    Printf.sprintf "\t%s:" end_label :: []
+  
+  | If (cmp_type, (r1, Int n), true_branch, false_branch) ->
+    let true_label = generate_if_label () in
+    let end_label = generate_if_label () in
+    let cmp_instruction =
+      if n <= 255 then
+        Printf.sprintf "\tcmp %s, #%d" r1 n
+      else
+        Printf.sprintf "\tldr r12, =%d\n\tcmp %s, r12" n r1
+    in
+    cmp_instruction ::
+    Printf.sprintf "\tb%s %s" cmp_type true_label ::
+    List.concat (List.map generate_asm_regt false_branch) @
+    Printf.sprintf "\tb %s" end_label ::
+    Printf.sprintf "\t%s:" true_label ::
+    List.concat (List.map generate_asm_regt true_branch) @
+    Printf.sprintf "\t%s:" end_label :: [] 
+  | Int n ->
+    if n <= 255 then
+      [Printf.sprintf "\tmov r0, #%d" n]
+    else
+      [Printf.sprintf "\tldr r0, =#%d" n]      
+  | Reg reg -> [Printf.sprintf "\tmov r0, %s" reg]
+  | Add (s1, expr) ->
+    (match expr with
+    | Int n -> 
+      if n <= 255 then
+        [Printf.sprintf "\tadd r0, %s, #%d" s1 n]
+      else
+        [Printf.sprintf "\tldr r0, =#%d" n; Printf.sprintf "\tadd r0, %s, r0" s1]
+    | Reg reg -> [Printf.sprintf "\tadd r0, %s, %s" s1 reg]
+    | _ -> assert false)
+  | Sub (s1, expr) ->
+    (match expr with
+    | Int n -> 
+      if n <= 255 then
+        [Printf.sprintf "\tsub r0, %s, #%d" s1 n]
+      else
+        [Printf.sprintf "\tldr r0, =#%d" n; Printf.sprintf "\tsub r0, %s, r0" s1]
+    | Reg reg -> [Printf.sprintf "\tsub r0, %s, %s" s1 reg]
+    | _ -> assert false)
+  | Call (func_name, nb_params) ->
+      [Printf.sprintf "\tpush {r0-r10}";
+        Printf.sprintf "\tbl %s" func_name;
+        Printf.sprintf "\tmov r12, r0";
+        Printf.sprintf "\tpop {r0-r10}";
+        Printf.sprintf "\tmov r0, r12"]
+  | Neg s1 -> [Printf.sprintf "\tneg r0, %s" s1]
+  | Unit -> []
+  | _ -> assert false)
+| Store (Reg reg, mem) -> [Printf.sprintf "\tstr %s, [fp, #%s]" reg mem]
+| Load (s, Reg reg) -> [Printf.sprintf "\tldr %s, [fp, #%s]" reg s]
+| Push (r) -> [Printf.sprintf "\tpush {%s}" r]
+| _ -> assert false
 
 and generate_asm_fun_internal : reg_function -> string list = fun { name; body } ->
   let rec extract_negative_sizes_regt : regt -> int list = function
